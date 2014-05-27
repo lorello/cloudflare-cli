6bd23c6be2574e92d178b86e2613689ca1f34344
lorello <lorenzo.salvadorini@softecspa.it>
2014-05-27 16:18:30 +0200

    Added parameter to purge a single file from CF cache

--- src/Cloudflare/Command/PurgeCacheCommand.php
+++ src/Cloudflare/Command/PurgeCacheCommand.php
@@ -13,8 +13,9 @@ class PurgeCacheCommand extends ContainerAwareCommand
     {
         parent::__construct($app, $name);
         $this->setDescription('Purge cache of a specific domain');
-        $this->setHelp('Purge cache of the speficied domain');
+        $this->setHelp('Purge cache of the specified domain');
         $this->addArgument('domain', Console\Input\InputArgument::REQUIRED, 'The domain to purge cache');
+        $this->addArgument('url', Console\Input\InputArgument::OPTIONAL, 'Remove from cache this specific URL');
     }
 
     protected function execute(Console\Input\InputInterface $input, Console\Output\OutputInterface $output)
@@ -23,20 +24,37 @@ class PurgeCacheCommand extends ContainerAwareCommand
 
         $data = $this->app['guzzle']->getData('cf');
 
-        $commandParams = array(
-            'u'   => $this->app['cf.user'],
-            'tkn' => $this->app['cf.token'],
-            'a'   => 'fpurge_ts',
-            'z'   => $domain,
-            'v'   => 1
-        );
+        $url = $input->getArgument('url');
+        $urlTextMsg = '';
+        if (!empty($url))
+        {
+            $commandParams = array(
+              'u'   => $this->app['cf.user'],
+              'tkn' => $this->app['cf.token'],
+              'a'   => 'zone_file_purge',
+              'z'   => $domain,
+              'url' => $url
+            );
+
+            $urlTextMsg = "URL '$url' on";
+
+        } else {
+
+            $commandParams = array(
+                'u'   => $this->app['cf.user'],
+                'tkn' => $this->app['cf.token'],
+                'a'   => 'fpurge_ts',
+                'z'   => $domain,
+                'v'   => 1
+            );
+        }
 
         $response = $this->app['guzzle']['cf']->CachePurge($commandParams);
 
         if ($response['result'] == 'error') {
-            $output->writeln("\n<error>Error purging domain $domain:\n\t$response[msg]</error>\n");
+            $output->writeln("\n<error>Error purging domain $urlTextMsg $domain:\n\t$response[msg]</error>\n");
         } else {
-            $output->writeln("\n<info>Successfuly purged domain $domain</info>\n");
+            $output->writeln("\n<info>Successfuly purged $urlTextMsg domain $domain</info>\n");
         }
         if (OutputInterface::VERBOSITY_DEBUG <= $output->getVerbosity()) {
             $output->writeln(var_dump($response->toArray()));
