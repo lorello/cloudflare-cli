c3481064d3b0bfea8bc240232440c4c133d6d492
lorello <lorenzo.salvadorini@softecspa.it>
2014-06-24 11:24:32 +0200

    better release script

--- release.sh
+++ release.sh
@@ -7,20 +7,27 @@ VERSION=$1
 
 [ -z $VERSION ] && echo "Missing version parameter" && exit 1
 
-
+[ -f cloudflare.phar ] && rm cloudflare.phar
 box build
+[ ! -f cloudflare.phar ] && echo "\nMissing cloudflare.phar, build failed?\n" && exit 1
 
-s3cmd cloudflare.phar s3://static.devops.it/cloudflare-cli/cloudflare-cli-{$VERSION}.phar
+# Public file
+s3cmd put cloudflare.phar s3://static.devops.it/cloudflare-cli/cloudflare-cli-{$VERSION}.phar
 
+# Create fragment for versions.json
 SHA1=$(php -r "echo sha1_file('cloudflare.phar');")
-echo -e "\n{"
-echo -e "    \"name\": \"cloudflare.phar\",\n"
-echo -e "    \"sha1\": $SHA1"
-echo -e ",\n"
+echo -e "\n  {"
+echo -e "    \"name\": \"cloudflare.phar\","
+echo -e "    \"sha1\": \"${SHA1}\","
 echo -e "    \"url\": \"http://static.devops.it/cloudflare-cli/cloudflare-cli-${VERSION}.phar\","
-echo -e "}\n"
-
+echo -e "    \"version\": ${VERSION},"
+echo -e "  }\n"
 
 git tag $VERSION
 
+echo "Manually update versions.json, then continue"
+read WAIT
+
+git commit -a -m "released version ${VERSION}"
+git push
 
