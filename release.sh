57ab473948d51d1317dfda82b8c470356db270ae
lorello <lorenzo.salvadorini@softecspa.it>
2014-06-24 11:18:14 +0200

    released version 0.5

--- release.sh
+++ release.sh
@@ -7,16 +7,20 @@ VERSION=$1
 
 [ -z $VERSION ] && echo "Missing version parameter" && exit 1
 
-#git tag $VERSION
 
 box build
 
 s3cmd cloudflare.phar s3://static.devops.it/cloudflare-cli/cloudflare-cli-{$VERSION}.phar
 
+SHA1=$(php -r "echo sha1_file('cloudflare.phar');")
 echo -e "\n{"
 echo -e "    \"name\": \"cloudflare.phar\",\n"
-echo -e "    \"sha1\": "
-php -r "echo sha1_file('cloudflare.phar');"
+echo -e "    \"sha1\": $SHA1"
 echo -e ",\n"
-echo -e "    \"url\": \"s3://static.devops.it/cloudflare-cli/cloudflare-cli-{$VERSION}.phar\","
+echo -e "    \"url\": \"http://static.devops.it/cloudflare-cli/cloudflare-cli-${VERSION}.phar\","
 echo -e "}\n"
+
+
+git tag $VERSION
+
+
