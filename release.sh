af5e445df63d606011faf74d66692be80f12bb5a
lorello <lorenzo.salvadorini@softecspa.it>
2014-06-25 19:43:10 +0200

    better release script

--- release.sh
+++ release.sh
@@ -6,8 +6,9 @@
 VERSION=$1
 [ -z $VERSION ] && echo "Missing version parameter" && exit 1
 
-ARTIFACT="cloudflare-cli-${VERSION}.phar"
-DOWNLOAD_NAME="cloudflare"
+ARTIFACT="cloudflare.phar"
+DOWNLOADABLE_NAME="cloudflare-cli-${VERSION}.phar"
+LOCAL_NAME="cloudflare"
 BUCKET="static.devops.it/cloudflare-cli"
 
 [ -f $ARTIFACT ] && rm $ARTIFACT
@@ -17,16 +18,15 @@ box build
 [ ! -f $ARTIFACT ] && echo "\nMissing $ARTIFACT, build failed?\n" && exit 1
 
 # Public file and clean current directory
-s3cmd put $ARTIFACT s3://$BUCKET/$ARTIFACT
-rm $ARTIFACT
+s3cmd put $ARTIFACT s3://$BUCKET/$DOWNLOADABLE_NAME
 
 # Create fragment for versions.json
 SHA1=$(php -r "echo sha1_file('$ARTIFACT');")
 echo -e "\n  {"
-echo -e "    \"name\": \"${DOWNLOAD_NAME}\","
+echo -e "    \"name\": \"${LOCAL_NAME}\","
 echo -e "    \"sha1\": \"${SHA1}\","
-echo -e "    \"url\": \"http://$BUCKET/$ARTIFACT\","
-echo -e "    \"version\": ${VERSION},"
+echo -e "    \"url\": \"http://$BUCKET/$DOWNLOADABLE_NAME\","
+echo -e "    \"version\": \"${VERSION}\","
 echo -e "  }\n"
 
 echo "Manually update versions.json, then continue"
@@ -35,3 +35,4 @@ read WAIT
 git commit -m "released version ${VERSION}" versions.json
 git push
 
+rm $ARTIFACT
